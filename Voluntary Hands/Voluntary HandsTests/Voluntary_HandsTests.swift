db7be43bdf888b741e1dffe9ec1c13006cf8c45c
Thiago Centurion <thiago.centurion@neon.com.br>
2020-04-09 22:49:21 -0300

    Adding project to repository.

---
+++ Voluntary Hands/Voluntary HandsTests/Voluntary_HandsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Voluntary_HandsTests.swift
+//  Voluntary HandsTests
+//
+//  Created by Thiago Rodrigues Centurion on 09/04/20.
+//  Copyright © 2020 Thiago Rodrigues Centurion. All rights reserved.
+//
+
+import XCTest
+@testable import Voluntary_Hands
+
+class Voluntary_HandsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
