95c6b1237a1970ce0d60279698475feccc0e3c2b
Thiago Centurion <thiago.centurion@neon.com.br>
2020-06-03 21:40:50 -0300

    Changing the project to use SwiftUI

--- Voluntary Hands/Voluntary Hands/AppDelegate.swift
+++ Voluntary Hands/Voluntary Hands/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Voluntary Hands
 //
-//  Created by Thiago Rodrigues Centurion on 09/04/20.
+//  Created by Thiago Rodrigues Centurion on 03/06/20.
 //  Copyright © 2020 Thiago Rodrigues Centurion. All rights reserved.
 //
 
--- Voluntary Hands/Voluntary Hands/SceneDelegate.swift
+++ Voluntary Hands/Voluntary Hands/SceneDelegate.swift
@@ -2,11 +2,12 @@
 //  SceneDelegate.swift
 //  Voluntary Hands
 //
-//  Created by Thiago Rodrigues Centurion on 09/04/20.
+//  Created by Thiago Rodrigues Centurion on 03/06/20.
 //  Copyright © 2020 Thiago Rodrigues Centurion. All rights reserved.
 //
 
 import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,7 +18,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
